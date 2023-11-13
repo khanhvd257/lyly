fced51a940a8949a87889768a037301ee8831392
Khanh Van Dinh <79238188+khanhvd2002@users.noreply.github.com>
2023-11-13 10:28:18 +0700

    Updates

---
+++ assets/lazy-93080ec6.js
@@ -0,0 +1,1 @@
+import{Y as r,y as n,C as u,A as f}from"./index-d8bdc496.js";const c=r({eager:Boolean},"lazy");function m(a,o){const e=n(!1),s=u(()=>e.value||a.eager||o.value);f(o,()=>e.value=!0);function t(){a.eager||(e.value=!1)}return{isBooted:e,hasContent:s,onAfterLeave:t}}export{c as m,m as u};

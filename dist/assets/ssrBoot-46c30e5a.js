bd785665244642c2d964039a5ed2d2dd3b3aeb99
KhanhVD <79238188+khanhvd2002@users.noreply.github.com>
2023-11-13 13:22:00 +0700

    Delete dist directory

--- dist/assets/ssrBoot-46c30e5a.js
+++
@@ -1,1 +0,0 @@
-import{y as t,J as e,C as s,b5 as n}from"./index-d8bdc496.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
